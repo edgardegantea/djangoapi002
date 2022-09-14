2ace51b5cce714f6d0b11410b139c79e1d091685
edgardegantea <edgar.degante.a@gmail.com>
2022-09-13 20:27:55 -0500

    api con Django

--- djangoapi002/urls.py
+++ djangoapi002/urls.py
@@ -1,26 +1,12 @@
-"""djangoapi002 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.urls import include, path
 from rest_framework import routers
-from api import views
+from api import views as vistas
 from django.contrib import admin
+from employee import views as ve
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
+router.register(r'users', vistas.UserViewSet)
+router.register('empleados', ve.Employee)
 
 
 urlpatterns = [
@@ -28,15 +14,3 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api/', include('rest_framework.urls', namespace='rest_framework'))
 ]
-
-
-
-
-
-
-# from employee import views
-# from rest_framework import routers
-
-# router = routers.DefaultRouter(trailing_slash=False)
-# router.register('employeedetails', views.Employee)
-
